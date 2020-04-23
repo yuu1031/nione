f6f590853d5aca1a60fc64ad2b720beb4f54c850
yuu1031 <yuxr1031nn@163.com>
2020-04-23 15:30:28 +0800

    数组相乘

---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int a[2][3];
+   int b[3][2];
+   int c[2][2];
+   int i,j,k;
+    for(i=0;i<2;i++)
+   {
+       for(j=0;j<3;j++)
+        cin>>a[i][j];
+   }
+     for(i=0;i<3;i++)
+   {
+       for(j=0;j<2;j++)
+        cin>>b[i][j];
+   }
+   for(i=0;i<2;i++)
+   {for(j=0;j<2;j++)
+    c[i][j]=0;
+   }
+   for(i=0;i<2;i++)
+   {
+       for(j=0;j<2;j++)
+       {for(k=0;k<3;k++)
+        c[i][j]+=a[i][k]*b[k][j];
+       }
+   }
+   for(i=0;i<2;i++)
+   {
+       for(j=0;j<2;j++)
+    {
+        cout<<c[i][j]<<" ";
+        if(j==1)
+            cout<<endl;
+    }
+
+   }
+}
