15a31c973b65bcac7daf917bbbab7cd06dab85a6
yuu1031 <yuxr1031nn@163.com>
2020-04-24 09:56:04 +0800

    7

--- 1.cpp
+++ 1.cpp
@@ -1,5 +1,6 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 #include<iostream> 
 using namespace std;
 void queen(char a[])
@@ -123,3 +124,139 @@ int main()
     return 0;
 >>>>>>> luo P5705
 }
+=======
+#include <iostream>
+using namespace std;
+const float pi=3.14159;
+class Shape
+/*1）私有成员m_ID
+
+2）公有getter和setter
+
+3）计算面积函数getArea()，返回0;
+
+4）构造与析构函数*/
+{
+public:
+    void setID(int x);
+    int  getID();
+    int getArea()//计算面积函数getArea()，返回0;
+    {
+        return 0;
+    }
+    Shape(int x);
+    ~Shape()
+    {
+        cout<<"Shape destructor called:"<<m_ID<<endl;
+    }
+private:
+    int m_ID;
+};
+class Circle:public Shape
+//Circle类从shape类继承，并派生以下成员
+/*1）私有成员r
+
+2）公有getter和setter
+
+3）计算面积函数getArea()，返回计算面积;
+
+4）构造与析构函数*/
+{
+public:
+    void setR(int x,int ID)
+    {
+        setID(ID);
+        getID();
+        r=x;
+    }
+    double getR()
+    {
+        return r;
+    }
+    double getArea()
+    {
+        double s;
+        s=pi*getR()*getR();
+        return s;
+    }
+    Circle(int x,int y):Shape(y)
+    {
+        r=x;
+        cout<<"Circle constructor called:"<<getID()<<endl;
+    }
+    ~Circle()
+    {
+        cout<<"Circle destructor called:"<<getID()<<endl;
+    }
+private:
+    int r;
+
+};
+class Rectangle:public Shape
+//Rectangle类从shape类继承，并派生以下成员
+/*1）私有成员h,w
+
+2）公有getter和setter
+
+3）计算面积函数getArea()，返回计算面积;
+
+4）构造与析构函数*/
+{
+public:
+    void setHW(int x,int y,int id)
+    {
+        setID(x);
+        getID();
+        h=x;
+        w=y;
+    }
+    int getH()
+    {
+        return h;
+    }
+    int getw()
+    {
+        return w;
+    }
+    int getArea()
+    {
+        return w*h;
+    }
+    Rectangle(int x,int y,int id):Shape(id)
+    {
+        h=x;
+        w=y;
+        cout<<"Rectangle constructor called:"<<getID()<<endl;
+    };
+    ~Rectangle()
+    {
+        cout<<"Rectangle destructor called:"<<getID()<<endl;
+    }
+private:
+    int h,w;
+};
+void Shape::setID(int x)
+{
+    m_ID=x;
+}
+int  Shape::getID()
+{
+    return m_ID;
+}
+Shape::Shape(int x)
+{
+    m_ID=x;
+    cout<<"Shape constructor called:"<<m_ID<<endl;
+}
+
+int main()
+{
+    Shape s(1);//1表示ID
+    Circle c(4,2);//4表示半径，2表示ID
+    Rectangle r(4, 5,3);//4和5表示长和宽，3表示ID
+    cout<<"Shape的面积"<<s.getArea()<<endl;
+    cout<<"Circle的面积"<<c.getArea()<<endl;
+    cout<<"Rectangle的面积"<<r.getArea()<<endl;
+}
+
+>>>>>>> 7
--- sourcecode.cpp
+++ sourcecode.cpp
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 //实现功能：既能排序整型数，也可以排序字符串
 
 #include <iostream>
@@ -145,5 +146,90 @@ int main()
 
 >>>>>>> 6(2)
 	return 0;
+=======
+#include "iostream"
+using namespace std;
+
+class vehicle
+{
+public:
+
+    void run()
+    {
+        cout<<"vehicle run"<<endl;
+    }
+    void stop()
+    {
+        cout<<"vehicle stop"<<endl;
+    }
+    vehicle(int x,int y)
+    {
+        maxspeed=x;
+        weight=y;
+        cout<<"vehicle constructor called.";
+        cout<<" maxspeed:"<<maxspeed<<"; weight"<<weight<<endl;
+    }
+    ~vehicle()
+    {
+        cout<<"vehicle destructor called.";
+        cout<<" maxspeed:"<<maxspeed<<"; weight"<<weight<<endl;
+    }
+private:
+    int maxspeed,weight;
+};
+class bicycle: virtual public vehicle
+//从vehicle派生bicycle和motorcar, bicycle有私有成员height， motorcar有私有成员seatnum,以及各自的构造函数和析构函数。
+{
+public:
+
+    bicycle(int x,int y,int m):vehicle(y,m)
+    {
+        height=x;
+        cout<<"bicycle constructor called. height:"<<height<<endl;
+    }
+    ~bicycle()
+    {
+        cout<<"bicycle destructor called. height:"<<height<<endl;
+    }
+private :
+    int height;
+};
+class motorcar: virtual public vehicle
+{
+public:
+    motorcar(int x,int y,int m):vehicle(y,m)
+    {
+        seatnum=x;
+        cout<<"motorcar constructor called. seatnum:"<<seatnum<<endl;
+    }
+    ~motorcar()
+    {
+        cout<<"motorcar destructor called. seatnum:"<<seatnum<<endl;
+    }
+private :
+    int seatnum;
+};
+class motorcycle: public bicycle,public motorcar
+//从bicycle和motorcar派生出motorcycle，观察虚基类对继承的影响。
+{
+public:
+    motorcycle(int x, int y, int m, int n):bicycle(x,m,n),motorcar(y,m,n),vehicle(m,n)
+    {
+        cout<<"motorcycle constructor called"<<endl;
+    }
+    ~motorcycle()
+    {
+        cout<<"motorcycle destructor called"<<endl;
+    }
+};
+
+
+int main(int argc, char *argv[])
+{
+    motorcycle m(1, 2, 3, 4);//1表示height，2表示setnum，3表示maxspeed，4表示weight
+    m.run();
+    m.stop();
+    return 0;
+>>>>>>> 7
 }
 
